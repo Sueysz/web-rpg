1087c2f6061b494a92d504539bb7b340f26e97aa
sueys <sueys@live.fr>
2023-10-24 13:39:58 +0200

    react test

---
+++ src/assets/components/Map.jsx
@@ -0,0 +1,13 @@
+
+export const Map = () => {
+
+    return (
+        <>
+            <div id="map" style={{width:"75%", height:"80%", backgroundColor:"black"}}>
+                <div className="tiles"></div>
+                <div className="tiles"></div>
+
+            </div>
+        </>
+    )
+}
